7614f3eed3c71eddb8c59784bcd7b1029bd3e30f
ryunosuke <ryunosuke.arima@gmail.com>
2022-03-22 01:36:34 +0900

    [demo] 更新

--- demo/run.sh
+++ demo/run.sh
@@ -2,4 +2,5 @@
 
 cd $(dirname $(dirname $(readlink -f $0)))
 
-php describe.phar mysql://127.0.0.1/test_describer demo/ --config demo/config.php
+php describe.phar mysql://127.0.0.1/test_describer demo/ --mode all  --config demo/config.php
+php describe.phar mysql://127.0.0.1/test_describer demo/ --mode html --config demo/config.php
